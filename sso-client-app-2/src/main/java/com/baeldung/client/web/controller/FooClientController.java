f57b65b2f4b96fb5a5884139e378cbb05edb05eb
Wilder Rojas <wrojas32@gmail.com>
2020-07-02 19:59:43 -0500

    Added get by id method

--- sso-client-app-2/src/main/java/com/baeldung/client/web/controller/FooClientController.java
+++ sso-client-app-2/src/main/java/com/baeldung/client/web/controller/FooClientController.java
@@ -1,5 +1,6 @@
 package com.baeldung.client.web.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.baeldung.client.web.model.FooModel;
@@ -21,14 +23,28 @@ public class FooClientController {
     @Autowired
     private WebClient webClient;
 
+    @GetMapping("/foos/{id}")
+    public String findOne(@PathVariable Long id, Model model) {
+        FooModel foo = this.webClient.get()
+            .uri(fooApiUrl+"/"+id)
+            .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<FooModel>() {
+            })
+            .block();
+        List<FooModel> foos = new ArrayList<>();
+        foos.add(foo);
+        model.addAttribute("foos", foos);
+        return "foos";
+    }
+
     @GetMapping("/foos")
     public String getFoos(Model model) {
         List<FooModel> foos = this.webClient.get()
-            .uri(fooApiUrl)
-            .retrieve()
-            .bodyToMono(new ParameterizedTypeReference<List<FooModel>>() {
-            })
-            .block();
+                .uri(fooApiUrl)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<FooModel>>() {
+                })
+                .block();
         model.addAttribute("foos", foos);
         return "foos";
     }
